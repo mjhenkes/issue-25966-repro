5a6a13803c06dbe08b7e61efcd91acae048f0f83
mjhenkes <mjhenkes@gmail.com>
2023-03-01 16:04:37 -0600

    repro

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
-/// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
+  for (let index = 0; index < 1000; index++) {
+    it(`works ${index}`, () => {
+      cy.intercept("/orchestrator/v1/logout").as("postLogout");
+    })
+  }
 })
